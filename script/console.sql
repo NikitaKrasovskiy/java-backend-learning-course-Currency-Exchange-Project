cdf4f96d1d78192ee2672724f51294d9642600cf
cherigra <nikitakrasovskij5@gmail.com>
2023-04-20 08:05:51 +0300

    feat(resources): Добавил файл index.html fix(dataBase): убрал ошибку при INSERT INTI currencies  ERROR: column "full_name" of relation "currencies" does not exist fix(dataBase): испрвил таблицу exchange_rates а именно добавил автоинкремент в id (INT = SERIAL) feat(dataBase): добавил INSERT INTO для exchange_rates

--- script/console.sql
+++ script/console.sql
@@ -2,23 +2,32 @@ CREATE DATABASE CurrencyExchangeDB;
 
 CREATE TABLE currencies
 (
-    id       SERIAL PRIMARY KEY, -- Айди валюты, автоинкремент, первичный ключ
-    code     VARCHAR(128) UNIQUE NOT NULL, --Код валюты
+    id        SERIAL PRIMARY KEY,           -- Айди валюты, автоинкремент, первичный ключ
+    code      VARCHAR(128) UNIQUE NOT NULL, --Код валюты
     full_name VARCHAR(128)        NOT NULL, -- Полное имя валюты
-    sign     VARCHAR(128)        NOT NULL  --  Символ валюты
+    sign      VARCHAR(128)        NOT NULL  --  Символ валюты
 );
 
-DROP TABLE  currencies;
+DROP TABLE currencies;
 
 CREATE TABLE exchange_rates
 (
-    id               INT PRIMARY KEY,  -- Айди валюты, автоинкремент, первичный ключ
-    base_currency_id   INT REFERENCES currencies (id)  NOT NULL, --Код валюты
-    target_currency_id INT  REFERENCES currencies (id)    NOT NULL, -- Полное имя валюты
-    rate             DECIMAL NOT NULL,  --  Символ валюты
-    UNIQUE(base_currency_id, target_currency_id)
+    id                 SERIAL PRIMARY KEY,                         -- Айди валюты, автоинкремент, первичный ключ
+    base_currency_id   INT REFERENCES currencies (id) NOT NULL, --Код валюты
+    target_currency_id INT REFERENCES currencies (id) NOT NULL, -- Полное имя валюты
+    rate               DECIMAL                        NOT NULL, --  Символ валюты
+    UNIQUE (base_currency_id, target_currency_id)
 );
 
+DROP TABLE exchange_rates;
+
+INSERT INTO currencies (code, full_name, sign)
+VALUES ('AUD', 'Australian dollar', 'A$'),
+       ('USD', 'United States dollar', '$'),
+       ('EUR', 'Euro', '€');
+
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate)
+VALUES (1, 2,  0.99);
 -- TODO сделал вторую таблицу, исправил наимование таблиц
 
 
