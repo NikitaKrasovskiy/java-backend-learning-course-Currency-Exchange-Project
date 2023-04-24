9f539df9aef17562149f7d2a6f5292b8ef302c74
cherigra <nikitakrasovskij5@gmail.com>
2023-04-24 10:51:57 +0300

    feat(ExchangeRatesTargetService): добавил метод findByCodes (для ExchangeRatesTargetServlet)

--- src/main/java/com/CurrencyExchange/cherigra/service/ExchangeRatesTargetService.java
+++ src/main/java/com/CurrencyExchange/cherigra/service/ExchangeRatesTargetService.java
@@ -1,4 +1,27 @@
 package com.CurrencyExchange.cherigra.service;
 
+import com.CurrencyExchange.cherigra.dao.ExchangeRatesDao;
+import com.CurrencyExchange.cherigra.dto.ExchangeRatesDto;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class ExchangeRatesTargetService {
+    private static final ExchangeRatesService INSTANCE = new ExchangeRatesService();
+
+    private final ExchangeRatesDao currenciesDao = ExchangeRatesDao.getInstance();
+
+    public List<ExchangeRatesDto> findByCodes(String baseCode, String targetCode) {
+        return currenciesDao.findByCodes(baseCode, targetCode).stream()
+                .map(exchangeRates -> new ExchangeRatesDto(
+                        exchangeRates.getId(),
+                        exchangeRates.getBaseCurrencyId(),
+                        exchangeRates.getTargetCurrencyId(),
+                        exchangeRates.getRate()
+                )).collect(toList());
+    }
+    public static ExchangeRatesService getInstance() {
+        return INSTANCE;
+    }
 }
