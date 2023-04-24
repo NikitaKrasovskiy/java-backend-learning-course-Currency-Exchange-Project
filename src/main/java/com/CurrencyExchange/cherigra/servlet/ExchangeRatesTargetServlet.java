3ae02bef4b7c30d835b84902a04932777d227f8a
cherigra <nikitakrasovskij5@gmail.com>
2023-04-24 11:20:32 +0300

    feat(servlet(ExchangeRatesTargetService)):работает нужны тесты

--- src/main/java/com/CurrencyExchange/cherigra/servlet/ExchangeRatesTargetServlet.java
+++ src/main/java/com/CurrencyExchange/cherigra/servlet/ExchangeRatesTargetServlet.java
@@ -1,16 +1,21 @@
 package com.CurrencyExchange.cherigra.servlet;
 
+import com.CurrencyExchange.cherigra.dto.ExchangeRatesDto;
 import com.CurrencyExchange.cherigra.service.ExchangeRatesService;
 import com.CurrencyExchange.cherigra.service.ExchangeRatesTargetService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
+
+@WebServlet("/exchangeRate/*")
 public class ExchangeRatesTargetServlet  extends HttpServlet {
 
     private final ExchangeRatesTargetService exchangeRatesTargetService = ExchangeRatesTargetService.getInstance();
@@ -19,12 +24,12 @@ public class ExchangeRatesTargetServlet  extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String url = req.getPathInfo().replaceAll("/", "");
-        resp.setContentType("text/json");
-        resp.setCharacterEncoding(StandardCharsets.UTF_8.name()); // TODO добавить filter для сервлетов
-
+//        resp.setContentType("text/json");
+//        resp.setCharacterEncoding(StandardCharsets.UTF_8.name()); // TODO добавить filter для сервлетов
         String baseCurrencyCode = url.substring(0, 3);
         String targetCurrencyCode = url.substring(3);
+        var  byCodes = exchangeRatesTargetService.findByCodes(baseCurrencyCode, targetCurrencyCode);
 
-
+        mapper.writeValue(resp.getWriter(), byCodes);
     }
 }
