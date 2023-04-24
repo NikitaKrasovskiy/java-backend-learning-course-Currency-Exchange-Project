8933accc6ca74f2eb68e12626e6a31b45f197b44
cherigra <nikitakrasovskij5@gmail.com>
2023-04-24 10:47:54 +0300

    feat(ExchangeRatesDao): добавил  class ExchangeRatesTargetService

--- src/main/java/com/CurrencyExchange/cherigra/servlet/ExchangeRatesTargetServlet.java
+++ src/main/java/com/CurrencyExchange/cherigra/servlet/ExchangeRatesTargetServlet.java
@@ -1,4 +1,16 @@
 package com.CurrencyExchange.cherigra.servlet;
 
-public class ExchangeRatesTargetServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ExchangeRatesTargetServlet  extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
