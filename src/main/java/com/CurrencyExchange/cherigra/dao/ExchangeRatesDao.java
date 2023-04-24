b8aab020b965033ec6a107ea4b602c81b9e3c43c
cherigra <nikitakrasovskij5@gmail.com>
2023-04-24 08:01:23 +0300

    feat(ExchangeRatesDao): добавил SQL выборку FIND_ALL_SQL для findAll

--- src/main/java/com/CurrencyExchange/cherigra/dao/ExchangeRatesDao.java
+++ src/main/java/com/CurrencyExchange/cherigra/dao/ExchangeRatesDao.java
@@ -5,7 +5,24 @@ import com.CurrencyExchange.cherigra.entity.ExchangeRates;
 import java.util.List;
 import java.util.Optional;
 
-public class ExchangeRatesDao implements Dao<Integer, ExchangeRates>{
+public class ExchangeRatesDao implements Dao<Integer, ExchangeRates> {
+
+    private final String FIND_ALL_SQL = """
+            select ex_rates.id AS id,
+                    currencies_1.id AS base_id
+                    currencies_1.code AS base_code,
+                    currencies_1.full_name AS base_full_name
+                    currencies_1.sign AS base_sing,
+                    currencies_2.id AS target_id,
+                    currencies_2.code AS target_id_code,
+                    currencies_2.full_name AS target_full_name,
+                    currencies_2.sign AS target_sign
+                    ex_rates.rate AS rate
+                    from exchange_rates AS ex_rates
+                    JOIN currencies currencies_1 ON ex_rates.base_currency_id = currencies_1.id
+                    JOIN currencies currencies_2 ON ex_rates.target_currency_id = currencies_2.id
+            """;
+
     @Override
     public List<ExchangeRates> findAll() {
         return null;
