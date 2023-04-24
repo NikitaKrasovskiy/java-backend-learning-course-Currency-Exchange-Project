c34ae7e9246b69832eda06f90c313887a691d9ce
cherigra <nikitakrasovskij5@gmail.com>
2023-04-24 10:03:02 +0300

    fix(ExchangeRatesDao): FIND_ALL_SQL исправил была синтаксическая ошибка

--- src/main/java/com/CurrencyExchange/cherigra/dao/ExchangeRatesDao.java
+++ src/main/java/com/CurrencyExchange/cherigra/dao/ExchangeRatesDao.java
@@ -21,18 +21,18 @@ public class ExchangeRatesDao implements Dao<Integer, ExchangeRates> {
 
     private final String FIND_ALL_SQL = """
             select ex_rates.id AS id,
-                    currencies_1.id AS base_id
-                    currencies_1.code AS base_code,
-                    currencies_1.full_name AS base_full_name
-                    currencies_1.sign AS base_sing,
-                    currencies_2.id AS target_id,
-                    currencies_2.code AS target_id_code,
-                    currencies_2.full_name AS target_full_name,
-                    currencies_2.sign AS target_sign
-                    ex_rates.rate AS rate
-                    from exchange_rates AS ex_rates
-                    JOIN currencies currencies_1 ON ex_rates.base_currency_id = currencies_1.id
-                    JOIN currencies currencies_2 ON ex_rates.target_currency_id = currencies_2.id
+                                currencies_1.id AS base_id,
+                                currencies_1.code AS base_code,
+                                currencies_1.full_name AS base_full_name,
+                                currencies_1.sign AS base_sing,
+                                currencies_2.id AS target_id,
+                                currencies_2.code AS target_code,
+                                currencies_2.full_name AS target_full_name,
+                                currencies_2.sign AS target_sign,
+                                ex_rates.rate AS rate
+                                from exchange_rates AS ex_rates
+                                JOIN currencies currencies_1 ON ex_rates.base_currency_id = currencies_1.id
+                                JOIN currencies currencies_2 ON ex_rates.target_currency_id = currencies_2.id
             """;
 
     @Override
