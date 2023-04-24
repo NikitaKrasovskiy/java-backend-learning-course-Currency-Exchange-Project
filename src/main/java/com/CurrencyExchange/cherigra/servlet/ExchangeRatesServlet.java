faae1ce7214e8e53216be88ea581b13f67f71c60
cherigra <nikitakrasovskij5@gmail.com>
2023-04-24 09:46:10 +0300

    feat(prod(Servlet)): сделал сервлет /exchangeRates fiX(DataBase): error org.postgresql.util.PSQLException: ERROR: syntax error at or near "currencies_1"

--- src/main/java/com/CurrencyExchange/cherigra/servlet/ExchangeRatesServlet.java
+++ src/main/java/com/CurrencyExchange/cherigra/servlet/ExchangeRatesServlet.java
@@ -1,4 +1,30 @@
 package com.CurrencyExchange.cherigra.servlet;
 
-public class ExchangeRatesServlet {
+import com.CurrencyExchange.cherigra.dto.CurrenciesDto;
+import com.CurrencyExchange.cherigra.dto.ExchangeRatesDto;
+import com.CurrencyExchange.cherigra.service.CurrenciesService;
+import com.CurrencyExchange.cherigra.service.ExchangeRatesService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@WebServlet("/exchangeRates")
+public class ExchangeRatesServlet extends HttpServlet {
+
+    private final ExchangeRatesService exchangeRatesService = ExchangeRatesService.getInstance();
+    private final ObjectMapper mapper = new ObjectMapper();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/json");
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        List<ExchangeRatesDto> exchangeList = exchangeRatesService.findAll();
+        mapper.writeValue(resp.getWriter(), exchangeList);
+    }
 }
