ac0d77a75642129f66a67b29c1b2ec2288a1dab0
cherigra <nikitakrasovskij5@gmail.com>
2023-04-30 10:10:03 +0300

    refactor(prod): убрал комменты

--- src/main/java/com/CurrencyExchange/cherigra/servlet/CurrenciesServlet.java
+++ src/main/java/com/CurrencyExchange/cherigra/servlet/CurrenciesServlet.java
@@ -1,7 +1,6 @@
 package com.CurrencyExchange.cherigra.servlet;
 
 import com.CurrencyExchange.cherigra.dto.CurrenciesDto;
-import com.CurrencyExchange.cherigra.entity.Currencies;
 import com.CurrencyExchange.cherigra.service.CurrenciesService;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,6 +9,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -21,7 +21,7 @@ public class CurrenciesServlet extends HttpServlet {
 	
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 			List<CurrenciesDto> currencies = currenciesService.findAll();
 			mapper.writeValue(resp.getWriter(), currencies);
 	}
--- src/main/java/com/CurrencyExchange/cherigra/util/PropertiesUtil.java
+++ src/main/java/com/CurrencyExchange/cherigra/util/PropertiesUtil.java
@@ -6,8 +6,6 @@ import java.util.Properties;
 public final class PropertiesUtil {
 	
 	private static final Properties PROPERTIES = new Properties();
-	
-	
 	static {
 		loadProperties();
 	}
